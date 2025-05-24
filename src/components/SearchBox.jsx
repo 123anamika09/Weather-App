66307acff4e3b6b63f19e212b9cf0c9d15b98cc8
Anamika <anamika120502@gmail.com>
2025-05-24 16:42:34 +0530

    api url & key in safe folder

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -4,12 +4,11 @@ import Button from '@mui/material/Button';
 import"./SearchBox.css"
 export default function SearchBox({ updateInfo }){
     let [city,setCity] = useState("");
-    const API_URL = "https://api.openweathermap.org/data/2.5/weather";
-
-    const API_KEY = "3cc401c50ec54996e155cfd13dce5b13";
+   
 
     let getWeatherInfo = async()=>{
-         let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+
+         let response = await fetch(`${import.meta.env.VITE_API_URL}?q=${city}&appid=${import.meta.env.VITE_API_KEY}&units=metric`);
          let jsonResponse=   await response.json();
          console.log(jsonResponse);
          let result ={
